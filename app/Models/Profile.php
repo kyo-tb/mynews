ecdad48412cf4c2e6eef5c2b1c6164e25859c27d
名前 <>
2023-02-06 04:50:20 +0000

    No39 編集履歴を実装しよう

---
+++ app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    
+     // 以下を追記
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+    
+    // Process Modelに関連付けを行う
+    public function processes()
+    {
+        return $this->hasMany('App\Models\Process');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,34 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    Route::get('news/delete', 'delete')->name('news.delete');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('profile/create', 'add')->name('profile.create');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
+
+// use App\Http\Controllers\Admin\ProfileController;
+// Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+//     Route::get('profile/create', 'add')->middleware('auth');
+//     Route::get('profile/edit', 'edit')->middleware('auth');
+// });
 
 // use App\Http\Controllers\XXX\AAAController;
 // Route::controller(NewsController::class)->prefix('XXX')->group(function() {
 //     Route::get('XXX', 'bbb');
 // });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
